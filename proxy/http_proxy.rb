1e376aa62ed0db9abf39d7b4e6461ad69d29fee2
zhouminhao <zhouminhao@ziyuenet.com.cn>
2019-11-19 13:18:16 +0800

    Man-in-the-Middle

---
+++ proxy/http_proxy.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'bundler'
+Bundler.require :default
+
+proxy = EvilProxy::HTTPProxyServer.new Port: 8888
+
+proxy.before_request do |request|
+  puts request.body
+end
+
+proxy.before_response do |request, response|
+  puts response.body
+end
+
+trap "INT"  do proxy.shutdown end
+trap "TERM" do proxy.shutdown end
+
+proxy.start
